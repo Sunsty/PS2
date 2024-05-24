73e771dd3c19bc0662adc937e4994f8c9543baae
Sunsty <cailloux.baptiste@gmail.com>
2024-05-24 15:59:30 +0200

    Boss 2 Init + Pattern 1

--- Assets/Scripts/Bosses/Boss 2/Boss2Melee_Patterns.cs
+++ Assets/Scripts/Bosses/Boss 2/Boss2Melee_Patterns.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
+/// 
 /// /////////////////////// - TODO - ///////////////////////
 /// 
 /// - Pattern 1 -
@@ -18,6 +20,7 @@ using UnityEngine;
 /// 
 /// 
 /// ////////////////////////////////////////////////////////
+/// 
 /// </summary>
 
 public class Boss2Melee_Patterns : MonoBehaviour
@@ -25,27 +28,120 @@ public class Boss2Melee_Patterns : MonoBehaviour
     [Header("Imports")]
 
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] GameObject bossBar;
+    [SerializeField] GameObject mainCamera;
 
     [Header("Settings")]
 
     [SerializeField] float speed;
+    [SerializeField, Range(1,4)] int currentPattern;
+
+    [Header("Pattern 1")]
+
+    [SerializeField] GameObject[] pattern1BossWaypoints;
+
+    [SerializeField] float dashCdPattern1;
+    [SerializeField] float dashSpeed;
+    [SerializeField] int maxPattern1Count;
+
+    int pattern1Count;
+
+    [Header("Pattern 2")]
+
+
+
+    [Header("Pattern 3")]
+
+
 
     [Header("Private")]
 
     float clock;
+    int targetIndex;
 
     private void Start()
     {
-        
+        pattern1BossWaypoints = GameObject.FindGameObjectsWithTag("Boss 2 Melee Pattern 1 Waypoint").OrderBy(m => m.gameObject.transform.GetSiblingIndex()).ToArray();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        bossBar = GameObject.FindGameObjectWithTag("Boss Bar");
     }
 
     private void FixedUpdate()
     {
-        
+
+        ///////////////////// - Pattern 1 - /////////////////////
+
+        if (currentPattern == 1)
+        {
+            bossBar.SetActive(true);
+
+            mainCamera.GetComponent<Camera_Follow>().SwitchCameraBehavior(1);
+
+            if (clock <= 0)
+            {
+                clock = dashCdPattern1;
+            }
+
+            if (clock > 0)
+            {
+                clock -= Time.fixedDeltaTime;
+
+                if (clock < 0)
+                {
+                    pattern1Count++;
+                    targetIndex++;
+                    targetIndex = targetIndex % pattern1BossWaypoints.Length;
+                }
+            }
+
+            if (pattern1Count == maxPattern1Count)
+            {
+                currentPattern = 2;
+                clock = 0f;
+                pattern1Count = 0;
+                targetIndex = 0;
+            }
+        }
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 2 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 3 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
     }
 
     private void Update()
     {
-        
+        ///////////////////// - Pattern 1 - /////////////////////
+
+        if (currentPattern == 1)
+        {
+            float distance = Vector2.Distance(transform.position, pattern1BossWaypoints[targetIndex].transform.position);
+            transform.position = (Vector2.MoveTowards(transform.position, pattern1BossWaypoints[targetIndex].transform.position, dashSpeed * Time.deltaTime * distance));
+
+            Debug.DrawLine(transform.position, Vector2.MoveTowards(pattern1BossWaypoints[targetIndex].transform.position, transform.position, dashSpeed * Time.deltaTime * distance));
+        }
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 2 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 3 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
     }
 }
--- Assets/Scripts/Bosses/Boss 2/Boss2Range_Patterns.cs
+++ Assets/Scripts/Bosses/Boss 2/Boss2Range_Patterns.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 /// <summary>
+/// 
 /// /////////////////////// - TODO - ///////////////////////
 /// 
 /// - Pattern 1 -
@@ -19,34 +22,111 @@ using UnityEngine;
 ///     
 /// 
 /// ////////////////////////////////////////////////////////
+/// 
 /// </summary>
 
 public class Boss2Range_Patterns : MonoBehaviour
 {
-    [Header("Imports")]
+    [Header("Imports"), Space(10)]
 
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject mainCamera;
 
-    [Header("Settings")]
+    [Header("Settings"), Space(10)]
 
-    [SerializeField] float speed;
+    [SerializeField, Range(1,3)] int currentPattern;
 
-    [Header("Private")]
+    [Header("Pattern 1"), Space(10)]
+
+    [SerializeField] float speedPattern1;
+    [SerializeField] float orbitRange;
+    [SerializeField] float shootCdPattern1;
+
+    [Header("Pattern 2"), Space(10)]
+
+
+
+    [Header("Pattern 3"), Space(10)]
+
+
+
+    [Header("Private"), Space(10)]
 
     float clock;
 
     private void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     private void FixedUpdate()
     {
+        ///////////////////// - Pattern 1 - /////////////////////
 
+        if (currentPattern == 1)
+        {
+
+        }
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 2 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 3 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
     }
 
     private void Update()
     {
+        ///////////////////// - Pattern 1 - /////////////////////
 
+        if (currentPattern == 1)
+        {
+            mainCamera.GetComponent<Camera_Follow>().SwitchCameraBehavior(2);
+
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position = (transform.position - player.transform.position).normalized * orbitRange + player.transform.position;
+            transform.RotateAround(player.transform.position, Vector3.forward, Time.deltaTime * speedPattern1);
+
+            if (clock <= 0)
+            {
+                clock = shootCdPattern1;
+            }
+
+            if (clock > 0)
+            {
+                clock -= Time.deltaTime;
+
+                if (clock < 0)
+                {
+                    Debug.Log("Shoot");
+                }
+            }
+        }
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 2 - /////////////////////
+
+        if (currentPattern == 2)
+        {
+
+        }
+
+        /////////////////////////////////////////////////////////
+
+        ///////////////////// - Pattern 3 - /////////////////////
+
+
+
+        /////////////////////////////////////////////////////////
     }
 }
